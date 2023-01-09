0f3f3e21f190b70b3118332793cac5913410eda7
Josué <114172354+Josu88@users.noreply.github.com>
2023-01-09 19:53:31 +0100

    actualizado el getDB y initDB

--- db/initDB.js
+++ db/initDB.js
@@ -10,6 +10,9 @@ async function main() {
 
         console.log('Eliminando tablas en caso que existan...');
 
+        await connection.query(`DROP TABLE IF EXISTS user_unlike_news`);
+        await connection.query(`DROP TABLE IF EXISTS user_like_news`);
+        await connection.query(`DROP TABLE IF EXISTS news`);
         await connection.query(`DROP TABLE IF EXISTS user`);
 
         console.log('¡Tablas eliminadas!');
@@ -27,6 +30,40 @@ async function main() {
             )`
         );
 
+        await connection.query(
+            `CREATE TABLE IF NOT EXISTS news (
+                id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+                title VARCHAR(100) NOT NULL,
+                photo VARCHAR(255),
+                leadIn VARCHAR(100) NOT NULL,
+                text VARCHAR(255) NOT NULL,
+                theme VARCHAR(100) NOT NULL,
+                idUser INT UNSIGNED NOT NULL,
+                FOREIGN KEY (idUser) REFERENCES user(id)
+
+            )`
+        );
+
+        await connection.query(
+            `CREATE TABLE IF NOT EXISTS user_like_news (
+                id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+                idUser INT UNSIGNED NOT NULL,
+                idNews INT UNSIGNED NOT NULL,
+                FOREIGN KEY (idUser) REFERENCES user(id),
+                FOREIGN KEY (idNews) REFERENCES news(id)
+            )`
+        );
+
+        await connection.query(
+            `CREATE TABLE IF NOT EXISTS user_unlike_news (
+                id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+                idUser INT UNSIGNED NOT NULL,
+                idNews INT UNSIGNED NOT NULL,
+                FOREIGN KEY (idUser) REFERENCES user(id),
+                FOREIGN KEY (idNews) REFERENCES news(id)
+            )`
+        );
+
         console.log('¡Tablas creadas!');
 
         console.log('Insertando unos datos de prueba...');
@@ -34,6 +71,21 @@ async function main() {
         await connection.query(
             `INSERT INTO user (username, email, password)
              VALUES ('userPrueba', 'prueba@gmail.com', '123')`
+        );
+
+        await connection.query(
+            `INSERT INTO news (title, leadIn, text, theme,idUser)
+             VALUES ('NoticiaPrueba', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.', 'Lorem ipsum dolor sit','Actualidad',1)`
+        );
+
+        await connection.query(
+            `INSERT INTO user_like_news (id,idUser,idNews)
+             VALUES (1,1,1)`
+        );
+
+        await connection.query(
+            `INSERT INTO user_unlike_news (id,idUser,idNews)
+             VALUES (1,1,1)`
         );
 
         console.log('¡Datos de prueba insertados con éxito!');
