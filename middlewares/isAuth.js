3a568f462268d2c6f1e7e4b0b0af35833e420553
Josué <114172354+Josu88@users.noreply.github.com>
2023-01-10 00:07:55 +0100

    Actualizacion de middlewares

---
+++ middlewares/isAuth.js
@@ -0,0 +1,58 @@
+/* 
+    Middleware que comprobará si el usuario ha iniciado sesión o no
+*/
+
+const getDB = require('../db/getDB');
+const { generateError } = require('../helpers');
+const jwt = require('jsonwebtoken');
+require('dotenv').config(); // dependencia necesaria para leer la variable de entorno SECRET
+
+const isAuth = async (req, res, next) => {
+    let connection;
+
+    try {
+        connection = await getDB();
+
+        // Recuperar la cabecera de autorización
+        const { authorization } = req.headers;
+
+        // Si no existe la cabecera de autorizacion
+        if (!authorization) {
+            throw generateError('¡Falta la cabecera de autorización!', 401); // Unauthorized
+        }
+
+        // Variable que va a guardar la info del token
+        let tokenInfo;
+
+        try {
+            // Desencriptamos el token
+            tokenInfo = jwt.verify(authorization, process.env.SECRET);
+        } catch (error) {
+            throw generateError('¡El token no es válido!', 401); // Unauthorized
+        }
+
+        // Comprobamos que el id del usuario del token existe en la base de datos
+        const [user] = await connection.query(
+            `SELECT * FROM user WHERE id = ?`,
+            [tokenInfo.id]
+        );
+
+        // Si se ha eliminado el usuario
+        if (user.length < 1) {
+            throw generateError('¡El token no es válido!', 401); // Unauthorized
+        }
+
+        // Guardamos una propiedad nueva en el objeto req con la info del token
+        // el id del usuario que ha iniciado sesión
+        req.userAuth = tokenInfo;
+
+        // Si todo va bien, pasamos la pelota
+        next();
+    } catch (error) {
+        next(error);
+    } finally {
+        if (connection) connection.release();
+    }
+};
+
+module.exports = isAuth;
