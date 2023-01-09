519b3eb0edebe3325274f2e87ebf12cf529ff2ff
Josué <114172354+Josu88@users.noreply.github.com>
2023-01-09 19:25:56 +0100

    actualizar server.js

--- server.js
+++ server.js
@@ -23,8 +23,21 @@ app.use(fileUpload());
 /*   ### Controladores de Usuarios ###  */
 
 const newUser = require('./controllers/users/newUser');
-
 const loginUser = require('./controllers/users/loginUser');
+
+/*   ### Controladores de News ###  */
+
+const newNews = require('./controllers/News/newNews');
+const listUserNews = require('./controllers/News/listUserNews');
+const photoNews = require('./controllers/News/addNewsPhoto');
+const listNews = require('./controllers/News/listNews');
+const listFilterUserNews = require('./controllers/News/listFilterNews');
+
+/*   ### Middlewares ###  */
+
+// Middleware de validación de usuario
+const isAuth = require('./middlewares/isAuth');
+const { application } = require('express');
 
 /*   ### Endpoints Usuarios ###  */
 
@@ -33,6 +46,23 @@ app.post('/register', newUser);
 
 // Login de usuario
 app.post('/login', loginUser);
+
+/*   ### Endpoints News ###  */
+
+// Registrar nueva Noticia
+app.post('/newNews', isAuth, newNews);
+
+// Listar las Noticias de un usuario
+app.get('/listUserNews', isAuth, listUserNews);
+
+// Listar todas las noticias
+app.get('/listNews', listNews);
+
+// Listar Noticias filtradas por tema
+app.get('/listFilterNews', listFilterUserNews);
+
+// Añadir la photo de la Noticia
+app.post('/addPhoto', isAuth, photoNews);
 
 // Middleware de Error
 app.use((error, req, res, _) => {
