f1e4faacebf50c8c9074e8e7e863c461d7ac3ad5
Josué <114172354+Josu88@users.noreply.github.com>
2023-02-15 17:37:30 +0100

    AuthSchema.js modificado para que funcione

--- schemas/AuthSchema.js
+++ schemas/AuthSchema.js
@@ -4,23 +4,15 @@
 
 const Joi = require('joi');
 
-const authSchema = Joi.object().keys({
-    authorization: Joi.string()
-        .required()
-        .length(137)
-        .regex(/[A-Za-z0-9]/)
-        .error((errors) => {
-            if (
-                errors[0].code === 'any.required' ||
-                errors[0].code === 'string.empty'
-            ) {
-                return new Error('¡El authorization es requerido!');
-            } else {
-                return new Error(
-                    '¡El authorization debe tener 137 caracteres de longitud!'
-                );
-            }
-        }),
+const authSchema = Joi.object().error((errors) => {
+    if (
+        errors[0].code === 'any.required' ||
+        errors[0].code === 'string.empty'
+    ) {
+        return new Error('¡El authorization es requerido!');
+    } else {
+        return new Error('¡El authorization debe ser un objeto!');
+    }
 });
 
 module.exports = authSchema;
